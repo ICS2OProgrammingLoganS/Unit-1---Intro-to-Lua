900356603488c7b324b70e77de0a005f33579bd0
s278599 <logan.sweeney@ocsbstudent.ca>
2019-09-24 13:35:03 -0400

    Added Area and Hello world

--- AreaRecCircLoganS/main.lua
+++ AreaRecCircLoganS/main.lua
@@ -12,6 +12,21 @@ local heightOfRectangle = 200
 local areaOfRectangleh
 
 -- set the background colour
-display.setdefault ("background",  )
+display.setDefault ("background", 0/255, 100/255, 255/255)
 
 display.setStatusBar (display.HiddenStatusBar)
+
+myRectangle = display.newRect (0, 0, widthOfRectangle, heightOfRectangle)
+
+myRectangle.anchorX = 0
+myRectangle.anchorY = 0
+myRectangle.x = 20
+myRectangle.y = 20
+
+myRectangle.strokeWidth = 30
+
+myRectangle:setFillColor (0/255, 255/255, 0/255)
+
+myRectangle:setStrokeColor (255/255, 0/255, 0/255)
+
+areaOfRectangle = widthOfRectangle
