2cbeeb94a133b0ae6fec18f95d7de1b54ed6b238
s278599 <logan.sweeney@ocsbstudent.ca>
2019-10-03 14:05:26 -0400

    TouchAndReact
    oof

--- TouchandReactLoganS/main.lua
+++ TouchandReactLoganS/main.lua
@@ -8,3 +8,22 @@ display.setDefault ("background", 255/255, 0/255, 0/255)
 
 --hide status bar
 display.setStatusBar (display.HiddenStatusBar)
+
+--create first button, set its position and make it visible
+local blackButton = display.newImageRect ("Images/black-button.png", 150, 150)
+blackButton.x = display.contentWidth/2
+blackButton.y = display.contentHeight/2
+blackButton.isVisible = true
+
+--create second button, set position and make invisible
+local greenButton = display.newImageRect ("Images/green-button.png", 150, 150)
+greenButton.x = display.contentWidth/2
+greenButton.y = display.contentHeight/2
+greenButton.isVisible = false
+
+--create text object, set position, make invisible
+local textObject = display.newText ("You got me!", 0, 0, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = display.contentHeight/2
+textObject:setTextColor (1, 1, 0)
+textObject.isVisible = false
